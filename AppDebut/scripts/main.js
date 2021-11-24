5d9f3ab26c77e062237ef1e88c92c3c00bad8c5e
hackzak <zakariya.ozkaraca@gmail.com>
2021-11-24 10:39:53 +0300

    first commit

--- AppDebut/scripts/main.js
+++ AppDebut/scripts/main.js
@@ -1,14 +1,43 @@
 
-const CLEAPI = '6bafa7f8ee710026d36930664630bd19';
+const CLEFAPI = '6bafa7f8ee710026d36930664630bd19';
 let resultaAPI;
+
+const temps = document.querySelector('.temps')
+const temperature = document.querySelector('.temperature')
+const localisation = document.querySelector('.localisation')
 
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
 
         console.log(position);
+        let long = position .coords.longitude;
+        let lat = position.coords.latitude;
+        AppelAPI(long, lat);
 
     }, () => {
         alert(`Vous avez refusé la geolocalisation, l'applicatino ne peux pas 
-        fondtionner, veuillez l'activer.!`)
+        fonctionner, veuillez l'activer.!`)
     })
+}
+
+function AppelAPI(long, lat) {
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutelyunits=metric&lang=fr&appid=${CLEFAPI}`)
+    .then((reponse) => {
+        return reponse.json();
+    })
+    
+    .then((data) => {
+        console.log(data);
+
+        resultaAPI = data;
+
+        temps.innerText = resultaAPI.current.weather[0].description;
+        temperature.innerText = `${Math.trunc(resultaAPI.current.temp)}°`
+        localisation.innerText = resultaAPI.timezone;
+
+    })
+
+    
+
 }
